182479c385d41bfe8630129f882e79fda2e54c65
jkurtzman1 <jasonkurtzman@yahoo.com>
2022-12-05 22:46:32 -0500

    Make Filter work
    Changed the filter so it always re-renders on change, and sets filter to be a const. No need to separate initial filter state from previous filter states or next filter states.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>
+{
+  const [people, setPeople] = useState([{name: "Arto Hellas", number: '222-222-2222'}]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filterName, setFilterName] = useState('');
+
+  //No need to filter anything with emptry string, which is default
+  const toShow = people.filter(p => p.name.includes(filterName)).map(p => <li key={p.name}>{p.name}: {p.number}</li>);
+  
+  const addPerson = (event) =>
+  {
+    event.preventDefault();
+    const newPerson = {name: newName, number: newNumber};
+    for(let i = 0; i < people.length; i++)
+    {
+      if(people[i].name === newPerson.name)
+      {
+        alert(`${newPerson.name} is already in the phonebook!`);
+        return;
+      }
+    }
+    setPeople(people.concat(newPerson))
+    setNewName('');
+    setNewNumber('');
+  }
+
+  const handleNameChange = (event) =>
+  {
+    setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) =>
+  {
+    setNewNumber(event.target.value);
+  }
+
+  const handleFilterChange = (event) =>
+  {
+    setFilterName(event.target.value);
+    //console.log(people.filter(person => person.name.includes(filterName)));
+    //toShow = people.filter(person => person.name.includes(filterName));
+    //toShow = toShow.map(p => <li key={p.name}>{p.name}: {p.number}</li>)
+    //console.log(toShow);
+    //.map(person => <li key={person.name}>{person.name}: {person.number}</li>)
+
+  }
+
+  return(
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        <p>Filter By Name: <input value={filterName} onChange={handleFilterChange} /></p>
+      </div>
+      <h2>Add a New Person</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          Number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type='submit'>Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {toShow}
+      </ul>
     </div>
   );
 }
